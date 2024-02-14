0cc64a15ddc81ebf1771131081451812b1fa252f
BuildTools <progamertr8090@gmail.com>
2024-02-14 22:00:59 +0300

    improvement

--- src/protocol.h
+++ src/protocol.h
@@ -5,3 +5,13 @@ typedef struct basic_input {
     uint8_t packet_id;
     uint8_t input_cmd;
 } basic_input;
+
+typedef struct sidestick_input {
+    uint8_t packet_id;
+    uint16_t x_axis;
+    uint16_t y_axis;
+    uint16_t z_axis;
+    uint8_t view;
+    uint8_t throttle;
+    uint16_t buttons;
+} sidestick_input;
